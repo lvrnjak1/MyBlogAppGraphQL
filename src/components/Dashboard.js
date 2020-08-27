242c44f916dfe7d8ae62321a5c8e3b64c2e74238
lvrnjak1 <lvrnjak1@etf.unsa.ba>
2020-08-27 13:02:55 +0200

    cleaned up the dasboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,11 +1,10 @@
-import React, { useEffect } from "react";
+import React from "react";
 import GridList from "@material-ui/core/GridList";
 import GridListTile from "@material-ui/core/GridListTile";
 import Post from "./Post.js";
 import NewPost from "./NewPost";
 import * as Constants from "../constants/Constants.js";
-import { useQuery, withApollo, Query } from "react-apollo";
-import { useState } from "react";
+import { withApollo, Query } from "react-apollo";
 import Header from "./Header.js";
 import Grid from "@material-ui/core/Grid";
 import Container from "@material-ui/core/Container";
@@ -13,42 +12,6 @@ import "../css/style.css";
 import Search from "./Search.js";
 
 function Dashboard(props) {
-  //const [feedPosts, setFeedPosts] = useState([]);
-
-  // const { subscribeToMore, refetch, data } = useQuery(Constants.POPULATE_FEED, {
-  //   onCompleted(data) {
-  //     setFeedPosts(data.posts);
-  //   },
-  // });
-
-  //const { data, loading } = useSubscription(Constants.SUBSCRIBE);
-
-  //if (data) console.log(data);
-
-  // useEffect(() => {
-  //   subscribeToMore({
-  //     document: Constants.SUBSCRIBE,
-  //     updateQuery: (prev, { subscriptionData }) => {
-  //       if (!subscriptionData.data) return prev;
-  //       const newFeedItem = subscriptionData.data.newPost;
-  //       return Object.assign({}, prev, {
-  //         posts: [newFeedItem, ...prev.posts],
-  //       });
-  //       console.log(newFeedItem);
-  //       console.log(prev);
-  //       return prev;
-  //     },
-  //   });
-  // }, []);
-
-  // useEffect(() => {
-  //   if (data) {
-  //     console.log(data);
-  //     console.log("Here");
-  //     setFeedPosts(data.posts);
-  //   }
-  // }, [data]);
-
   const subscribeToNewPosts = (subscribeToMore) => {
     subscribeToMore({
       document: Constants.SUBSCRIBE,
